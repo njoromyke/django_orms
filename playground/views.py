cfbdf8052925c00abf343127a7f56c2722772d8d
nghaika01 <mykenjoroge01@gmail.com>
2022-07-06 06:37:24 +0300

    adding custom fields

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models.aggregates import Max, Avg, Min, Sum, Count
-from store.models import Product
+from django.db.models import Value,F
+from store.models import Customer, Product
 
 
 def say_hello(request):
-    result = Product.objects.filter('collection__id').aggregate(count=Count('id'),min_price =Min('unit_price'))
+    query_set = Customer.objects.annotate(new_id=F('id'))
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': query_set})
