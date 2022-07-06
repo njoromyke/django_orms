e873bcf0082dc19a85e84f7c475596c696e92f70
nghaika01 <mykenjoroge01@gmail.com>
2022-07-06 06:29:29 +0300

    filtering using aggregates

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,6 @@ from store.models import Product
 
 
 def say_hello(request):
-    result = Product.objects.aggregate(count=Count('id'),min_price =Min('unit_price'))
+    result = Product.objects.filter('collection__id').aggregate(count=Count('id'),min_price =Min('unit_price'))
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
