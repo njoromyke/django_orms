3855e282c7f7c73b9061a6a94265281133b0c158
nghaika01 <mykenjoroge01@gmail.com>
2022-07-06 06:43:38 +0300

    concat for fist name and last name

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Value,F
+from django.db.models import Value, F, Func
 from store.models import Customer, Product
 
 
 def say_hello(request):
-    query_set = Customer.objects.annotate(new_id=F('id') +1)
+    query_set = Customer.objects.annotate(
+        full_name=Func(F('first_name'), Value(" "), F('last_name'),function='CONCAT'))
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': query_set})
+ 
