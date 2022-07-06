0e82fb6085112cae571307b751fc125b30600250
nghaika01 <mykenjoroge01@gmail.com>
2022-07-06 08:57:25 +0300

    grouping data

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.db.models.functions import Concat
 from store.models import Customer, Product
 
 
 def say_hello(request):
     query_set = Customer.objects.annotate(
-        full_name=Func(F('first_name'), Value(" "), F('last_name'), function='CONCAT'))
-
-    query_set = Customer.objects.annotate(
-        full_name=Concat('first_name', Value(' '), 'last_name'))
+        orders_count=Count('order')
+    )
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': query_set})
