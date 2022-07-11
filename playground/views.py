72f0dda4b57cfae832c4f129905800bc51f78942
nghaika01 <mykenjoroge01@gmail.com>
2022-07-11 21:14:10 +0300

    deleting objects

--- playground/views.py
+++ playground/views.py
@@ -3,10 +3,7 @@ from store.models import Collection, Product
 
 
 def say_hello(request):
-    # collection = Collection.objects.get(pk=11)
-    # collection.featured_product = None
-    # collection.save()
-    
-    Collection.objects.filter(pk=11).update(featured_product=None)
+    collection = Collection(pk=4)
+    collection.delete()
 
     return render(request, 'hello.html', {'name': 'Mosh'})
