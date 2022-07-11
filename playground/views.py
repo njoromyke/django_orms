606cb204fafa71926552ca8a30d0eefb4fc80225
nghaika01 <mykenjoroge01@gmail.com>
2022-07-11 20:47:37 +0300

    saving records

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Collection, Product
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    list(query_set)
-    list(query_set)
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=1)
+    collection.save()
+    
 
     return render(request, 'hello.html', {'name': 'Mosh', 'tags': list(query_set)})
