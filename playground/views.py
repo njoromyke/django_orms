3bad60f63398bd906bf3d193d56091e427c1d14a
nghaika01 <mykenjoroge01@gmail.com>
2022-07-11 21:38:43 +0300

    executing raw sql queries

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Collection, Order, OrderItem, Product
 
 
 def say_hello(request):
-    query_set = Product.objects.raw('SELECT id,title FROM store_product')
+    with connection.cursor() as cursor:
+        cursor.callproc('get-customers', 1, 2, 3, 'a')
+
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(query_set)})
