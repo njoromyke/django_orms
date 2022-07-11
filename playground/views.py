4a82f29b4bea9ad9872a668e071613baad4730ac
nghaika01 <mykenjoroge01@gmail.com>
2022-07-11 21:12:16 +0300

    correct update statement

--- playground/views.py
+++ playground/views.py
@@ -3,9 +3,10 @@ from store.models import Collection, Product
 
 
 def say_hello(request):
-    collection = Collection(pk=11)
-    collection.title = 'Games'
-    collection.featured_product = None
-    collection.save()
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = None
+    # collection.save()
+    
+    Collection.objects.filter(pk=11).update(featured_product=None)
 
     return render(request, 'hello.html', {'name': 'Mosh'})
