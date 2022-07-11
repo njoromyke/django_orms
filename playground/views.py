8ada48d499cd91e5c99994da8323d5ba64172730
nghaika01 <mykenjoroge01@gmail.com>
2022-07-11 20:42:28 +0300

    caching mechanisms

--- playground/views.py
+++ playground/views.py
@@ -6,12 +6,8 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    content_type = ContentType.objects \
-        .get_for_model(Product)
-    query_set = TaggedItem.objects\
-        .select_related('tag') \
-        .filter(
-            content_type=content_type,
-            object_id=1
-        )
+    query_set = Product.objects.all()
+    list(query_set)
+    list(query_set)
+
     return render(request, 'hello.html', {'name': 'Mosh', 'tags': list(query_set)})
