49f5d457939c3186ca7cd4ae2d4cadd1f6ee7342
nghaika01 <mykenjoroge01@gmail.com>
2022-07-12 10:32:22 +0300

    one way to customize list page

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+
+
 admin.site.register(models.Collection)
 admin.site.register(models.Product)
