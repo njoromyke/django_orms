5e8309da8e93731dc334574aa88029527e465ef9
nghaika01 <mykenjoroge01@gmail.com>
2022-07-13 12:57:22 +0300

    implementing custom filters

--- store/admin.py
+++ store/admin.py
@@ -6,11 +6,26 @@ from django.urls import reverse
 from . import models
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title: str = 'inventory'
+    parameter_name: str = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
 
