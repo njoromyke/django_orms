dc5adec8a4fc11c23e00c7e05dae7f3bca457659
nghaika01 <mykenjoroge01@gmail.com>
2022-07-13 12:39:59 +0300

    Added customer orders field

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,8 @@
 from itertools import product
 from django.contrib import admin
 from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 
@@ -29,11 +31,24 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
 
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -41,7 +56,14 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (reverse('admin:store_product_changelist') + '?'
+               + urlencode({
+                   'collection__id': str(collection.id)
+               })
+
+
+               )
+        return format_html('<a href="{}"> {} </a>', url, collection.products_count)
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
